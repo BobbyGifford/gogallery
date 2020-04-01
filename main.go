2b3a5bea39b7e9bb18f9ad694932e444132b2181
Robert Gifford <robertgifford@Roberts-MacBook-Pro.local>
2020-04-01 15:37:01 -0700

    contact view

--- main.go
+++ main.go
@@ -2,14 +2,16 @@ package main
 
 import (
 	"fmt"
-	"github.com/gorilla/mux"
 	"html/template"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 var homeTemplate *template.Template
+var contactTemplate *template.Template
 
-func home(w http.ResponseWriter, r *http.Request){
+func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	err := homeTemplate.Execute(w, nil)
 	if err != nil {
@@ -17,17 +19,20 @@ func home(w http.ResponseWriter, r *http.Request){
 	}
 }
 
-func contact(w http.ResponseWriter, r *http.Request){
+func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	_, _ = fmt.Fprint(w, "<h1>Contact page</h1>")
+	err := contactTemplate.Execute(w, nil)
+	if err != nil {
+		panic(err)
+	}
 }
 
-func faq(w http.ResponseWriter, r *http.Request){
+func faq(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	_, _ = fmt.Fprint(w, "<h1>FAQ</h1>")
 }
 
-func notFound(w http.ResponseWriter, r *http.Request){
+func notFound(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	w.WriteHeader(http.StatusNotFound)
 	_, _ = fmt.Fprint(w, "<h1>Page not found</h1>")
@@ -36,6 +41,11 @@ func notFound(w http.ResponseWriter, r *http.Request){
 func main() {
 	var err error
 	homeTemplate, err = template.ParseFiles("views/home.gohtml")
+	if err != nil {
+		panic(err)
+	}
+
+	contactTemplate, err = template.ParseFiles("views/contact.gohtml")
 	if err != nil {
 		panic(err)
 	}

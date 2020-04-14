0e2594b8eb08d2a4ab6bb57fccdc4c35e49a0784
Robert Gifford <robertgifford@Roberts-MacBook-Pro.local>
2020-04-14 16:14:24 -0700

    Cleaned and refactored view logic

--- extra/main.go
+++ extra/main.go
@@ -1,26 +1,33 @@
 package main
 
-import (
-	"html/template"
-	"os"
-)
+import "fmt"
 
 type User struct {
 	Name string
 }
 
 func main() {
-	t, err := template.ParseFiles("hello.gohtml")
-	if err != nil {
-		panic(err)
-	}
+	//t, err := template.ParseFiles("hello.gohtml")
+	//if err != nil {
+	//	panic(err)
+	//}
+	//
+	//data := User{
+	//	Name: "John Smith",
+	//}
+	//
+	//err = t.Execute(os.Stdout, data)
+	//if err != nil {
+	//	panic(err)
+	//}
+	var cheeses = []string{"cheddar", "pep pep"}
+	cheese(cheeses)
+}
 
-	data := User{
-		Name: "John Smith",
-	}
+//func cheese(cheeses ...string) {
+//	fmt.Println(cheeses)
+//}
 
-	err = t.Execute(os.Stdout, data)
-	if err != nil {
-		panic(err)
-	}
+func cheese(cheeses [] string) {
+	fmt.Println(cheeses)
 }

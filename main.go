11608dc601da0d967d1c16b4848692f99036320b
Bobby Gifford <bobby077joe@gmail.com>
2020-04-15 18:42:45 -0700

    Clean up

--- main.go
+++ main.go
@@ -2,27 +2,11 @@ package main
 
 import (
 	"calhoun/controllers"
-	"calhoun/views"
 	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
-
-var (
-	homeView    *views.View
-	contactView *views.View
-)
-
-func home(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	must(homeView.Render(w, nil))
-}
-
-func contact(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	must(contactView.Render(w, nil))
-}
 
 func notFound(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
@@ -31,23 +15,16 @@ func notFound(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	homeView = views.NewView("bootstrap", "views/home.gohtml")
-	contactView = views.NewView("bootstrap", "views/contact.gohtml")
+	staticControllers := controllers.NewStatic()
 	usersController := controllers.NewUsers()
 
 	fmt.Println("Live on http://localhost:3000/")
 
 	r := mux.NewRouter()
 	r.NotFoundHandler = http.HandlerFunc(notFound)
-	r.HandleFunc("/", home).Methods("GET")
-	r.HandleFunc("/contact", contact)
+	r.Handle("/", staticControllers.Home).Methods("GET")
+	r.Handle("/contact", staticControllers.Contact).Methods("GET")
 	r.HandleFunc("/signup", usersController.New).Methods("GET")
 	r.HandleFunc("/signup", usersController.Create).Methods("POST")
 	_ = http.ListenAndServe(":3000", r)
-}
-
-func must(err error) {
-	if err != nil {
-		panic(err)
-	}
 }
